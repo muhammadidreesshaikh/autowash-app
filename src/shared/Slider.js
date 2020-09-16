6cac34d3d3fd785feb01e367352acf15f6f1dd8b
Muhammad Awais <awaisshaikh.official@gmail.com>
2020-09-17 00:27:56 +0500

    slider section start

--- src/shared/Slider.js
+++ src/shared/Slider.js
@@ -23,7 +23,21 @@ class Slider extends React.Component {
     render() {
         return(
             <div>
-                <p>Slider Slider Slider</p>
+               <section>
+                   <div className="slider">
+                       <div className="container">
+                           <div className="row">
+
+                               <div className="col-12 col-md-6 col-lg-6">
+                                   <div className="heading">
+                                    <h1>Say hello to best car wash platform</h1>
+                                    <p className="pt-3">simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </section>
             </div>
         );
     }

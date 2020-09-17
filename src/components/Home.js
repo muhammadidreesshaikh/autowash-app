2ce33ce38054bdece51c274bbe5427a9625749ba
Muhammad Awais <awaisshaikh.official@gmail.com>
2020-09-17 22:34:15 +0500

    check section and about section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Slider from '../shared/Slider';
 import '../assets/css/home.css';
+import about from '../assets/img/about.jpg'
+import check from '../assets/img/check.webp'
 // import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
@@ -27,10 +29,93 @@ class Home extends React.Component {
             <div>
                 <Slider />
                 <section>
-                    <p>Home Home Home</p>
+                    <div className="about">
+                        <div className="container">
+                            <div className="row">
 
-                    {/* <Link to="/contact">View All Deals</Link> */}
+                                <div className="col-12 col-md-6 col-lg-6">
+                                    <div className="image">
+                                            <img src={about} />
+                                    </div>
+                                </div>
+
+                                <div className="col-12 col-md-6 col-lg-6">
+                                    <div className="heading">
+                                        <h6>A B O U T</h6>
+                                        <h1 className="pt-2">About the Dirt2Clean</h1>
+                                        <p className="pt-3">simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum has been the standard dummy text ever since the since specimen book printing and typesetting industry.</p>
+
+                                        <div className="box">
+                                            <div className="row">
+
+                                                <div className="col-md-4 col-lg-4">
+                                                    <div className="card">
+                                                        <div className="card-body">
+                                                            <a href="#">i</a>
+                                                            <h4 className="pt-4">EXCLUSIVE</h4>
+                                                            <p>There are many variations of passages of Lorem Ipsum available. by injected humour.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="col-md-4 col-lg-4">
+                                                    <div className="card">
+                                                        <div className="card-body">
+                                                            <a href="#">i</a>
+                                                            <h4 className="pt-4">UPDATE</h4>
+                                                            <p>There are many variations of passages of Lorem Ipsum available. by injected humour.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="col-md-4 col-lg-4">
+                                                    <div className="card">
+                                                        <div className="card-body">
+                                                            <a href="#">i</a>
+                                                            <h4 className="pt-4">SECURE</h4>
+                                                            <p>There are many variations of passages of Lorem Ipsum available. by injected humour.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="check">
+                        <div className="container">
+                            <div className="row">
+
+                                <div className="col-12 col-md-6 col-lg-6">
+                                    <div className="heading">
+                                        <h6>We Bring You The Future</h6>
+                                        <h1 className="pt-2">Don't worries, we check all of unit</h1>
+                                        <p className="pt-3">simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum has been the standard dummy text ever since the since specimen book printing and typesetting industry.</p>
+
+                                        <div className="button pt-4">
+                                            <a href="#">Discover More</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-12 col-md-6 col-lg-6">
+                                    <div className="image">
+                                            <img src={check} />
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
                 </section>
+
+                {/* <Link to="/contact">View All Deals</Link> */}
             </div>
         );
     }
